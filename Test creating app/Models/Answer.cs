94d3116ad3d8f308034d7f7404bd6b48270ec2ed
Тарас Крупко <121-19-6-krupko@knute.edu.ua>
2020-11-25 01:44:18 +0200

    Добавил проект для создания тестов и переместил туда модели создания вопросов

---
+++ Test creating app/Models/Answer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_creating_app.Models
+{
+    class Answer
+    {
+        public Answer(string answer)
+        {
+            this._content = answer;
+            IsTrue = false;
+        }
+
+        private string _content;
+
+        public string Content
+        {
+            get
+            {
+                return _content;
+            }
+        }
+        public bool IsTrue { get; set; }
+    }
+}
---
+++ Test creating app/Models/QuestionPattern.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_creating_app.Models
+{
+    class QuestionPattern
+    {
+        public QuestionPattern(string question, string var1, string var2, string var3, string var4, string var5)
+        {
+            this._question = question;
+            this.FirstVar = new Answer(var1);
+            this.SecondVar = new Answer(var2);
+            this.ThirdVar = new Answer(var3);
+            this.FourthVar = new Answer(var4);
+            this.FifthVar = new Answer(var5);
+
+            AllAnswers = new List<Answer>(){FirstVar,SecondVar,ThirdVar,FourthVar,FifthVar};
+        }
+        public QuestionPattern(string question, string var1, string var2, string var3, string var4)
+        {
+            this._question = question;
+            this.FirstVar = new Answer(var1);
+            this.SecondVar = new Answer(var2);
+            this.ThirdVar = new Answer(var3);
+            this.FourthVar = new Answer(var4);
+
+            AllAnswers = new List<Answer>() { FirstVar, SecondVar, ThirdVar, FourthVar};
+        }
+        public QuestionPattern(string question, string var1, string var2, string var3)
+        {
+            this._question = question;
+            this.FirstVar = new Answer(var1);
+            this.SecondVar = new Answer(var2);
+            this.ThirdVar = new Answer(var3);
+
+            AllAnswers = new List<Answer>() { FirstVar, SecondVar, ThirdVar};
+        }
+        public QuestionPattern(string question, string var1, string var2)
+        {
+            this._question = question;
+            this.FirstVar = new Answer(var1);
+            this.SecondVar = new Answer(var2);
+
+            AllAnswers = new List<Answer>() { FirstVar, SecondVar};
+        }
+
+
+        private string _question;
+
+        public string Question
+        {
+            get { return _question; }
+        }
+
+        //Экземпляры класса Answer, которые содержат 2 свойства: Content и IsCorrect
+
+        public Answer FirstVar { get; set; }
+        public Answer SecondVar { get; set; }
+        public Answer ThirdVar { get; set; }
+        public Answer FourthVar { get; set; }
+        public Answer FifthVar { get; set; }
+
+        //Список со всеми ответами
+        public List<Answer> AllAnswers { get; }
+
+        public Answer CorrectAnswer
+        {
+            get
+            {
+                Answer iscorrect = new Answer("Правильного ответа не существует!");
+
+                foreach (var i in AllAnswers)
+                {
+                    if (i.IsTrue)
+                    {
+                        iscorrect = i;
+                        break;
+                    }
+                }
+
+                return iscorrect;
+            }
+        }
+
+    }
+}
