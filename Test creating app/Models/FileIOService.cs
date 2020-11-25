598bb3d7ac3face457089d0d9c3d7b457fd9d992
Тарас Крупко <121-19-6-krupko@knute.edu.ua>
2020-11-25 02:09:39 +0200

    Добавил сервис по сохранению и загрузке данных

---
+++ Test creating app/Models/FileIOService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Test_creating_app.Models
+{
+    class FileIOService
+    {
+        public FileIOService(string Path)
+        {
+            this._path = Path;
+        }
+
+        private readonly string _path;
+
+        public List<QuestionPattern> LoadData()
+        {
+            if (!File.Exists(_path))
+            {
+                File.Create(_path).Dispose();
+                return new List<QuestionPattern>();
+            }
+
+            using (StreamReader myReader = File.OpenText(_path))
+            {
+                var filetext = myReader.ReadToEnd();
+                return JsonConvert.DeserializeObject<List<QuestionPattern>>(filetext);
+            }
+        }
+
+        public void SaveData(List<QuestionPattern> questions)
+        {
+            using (StreamWriter streamWriter = File.CreateText(_path))
+            {
+                string questionsText = JsonConvert.SerializeObject(questions);
+                streamWriter.WriteLine(questionsText);
+            }
+        }
+    }
+}
